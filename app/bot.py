5e518aebbeb05f668c04d907e3aedb75315d7c9b
Ryan P. Daly <mathemaphysics@gmail.com>
2024-08-25 12:28:05 +0000

    Added error handling around loading of secrets.json.

--- app/bot.py
+++ app/bot.py
@@ -1,29 +1,55 @@
-import discord
 from pathlib import Path
+from discord import Intents
 from discord.ext import commands
 from json import load
 from log.botlog import BotLogger, INFO, ERROR, DEBUG
 
-# Setup for logging
-logger = BotLogger(__name__, DEBUG)
+# Flamingly global configuration stuff
+bot_name = "None"
+bot_token = ""
 
-# Secret
+# Secrets
 secrets_path = Path("./secrets.json")
-token = ""
+
+# Load stuff we need
 if secrets_path.exists():
+    # Make sure path exists, of course
     secrets_fp = secrets_path.open("r")
     json_data = load(secrets_fp)
-    token = json_data["discord"]["token"]
+
+    # Keys for consistency; they're gonna be used more than once
+    discord_key = "discord"
+    bindbot_key = "BindBot"
+    name_key = "name"
+    token_key = "token"
+
+    # Error checking
+    if discord_key not in json_data:
+        print(f"No '{discord_key}' key in {str(secrets_path)}")
+    elif bindbot_key not in json_data[discord_key]:
+        print(f"No '{bindbot_key}' key in {str(secrets_path)}")
+    else:
+        if name_key not in json_data[discord_key][bindbot_key]:
+            print(f"No '{name_key}' key in {str(secrets_path)}")
+        else:
+            bot_name = json_data[discord_key][bindbot_key][name_key]
+        if token_key not in json_data[discord_key][bindbot_key]:
+            print(f"No '{token_key}' key in {str(secrets_path)}")
+        else:
+            bot_token = json_data[discord_key][bindbot_key][token_key]
 else:
     raise Exception("Secrets path does not exist")
 
+# Setup for logging
+logger = BotLogger(bot_name, DEBUG)
+
 # Make sure token isn't empty
-if len(token) == 0:
+if len(bot_token) == 0:
     raise Exception("Token loaded but empty")
 
 # Define the bot's prefix (what you type before commands)
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents, )
+intents = Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Event: When the bot is ready
 @bot.event
@@ -41,4 +67,4 @@ async def hello(ctx):
     await ctx.send('Hello! How can I assist you today?')
 
 # Run the bot with your token
-bot.run(token)
+bot.run(bot_token)
