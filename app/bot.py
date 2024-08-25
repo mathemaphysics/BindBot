efc134abc627b048fb8d3c7a849f8bcf25a2a51e
Ryan P. Daly <mathemaphysics@gmail.com>
2024-08-25 09:03:55 +0000

    Created customized logger and updated secrets loading with error checking for completeness.

--- app/bot.py
+++ app/bot.py
@@ -1,16 +1,21 @@
-import os
 import discord
 from pathlib import Path
 from discord.ext import commands
 from json import load
-from logging import getLogger, Formatter, StreamHandler
+from log.botlog import BotLogger, INFO, ERROR, DEBUG
 
-# Logging configuration
-logger = getLogger(__name__)
+# Setup for logging
+logger = BotLogger(__name__, DEBUG)
 
 # Secret
 secrets_path = Path("./secrets.json")
-token = load(secrets_path.open("r"))["discord"]["token"] if secrets_path.exists() else ""
+token = ""
+if secrets_path.exists():
+    secrets_fp = secrets_path.open("r")
+    json_data = load(secrets_fp)
+    token = json_data["discord"]["token"]
+else:
+    raise Exception("Secrets path does not exist")
 
 # Define the bot's prefix (what you type before commands)
 intents = discord.Intents.all()
@@ -19,8 +24,7 @@ bot = commands.Bot(command_prefix='!', intents=intents, )
 # Event: When the bot is ready
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
-    print('------')
+    logger.info(f'Logged in as {bot.user.name} ({bot.user.id})')
 
 # Command: Respond to a simple ping command
 @bot.command()
---
+++ app/log/botlog.py
@@ -0,0 +1,13 @@
+import sys
+from logging import Logger, getLogger, Formatter, StreamHandler, INFO, ERROR, DEBUG
+
+class BotLogger(Logger):
+	def __init__(self, name: str = __name__, level: int = INFO):
+		# Initialize the base
+		super().__init__(name, level)
+
+		# Personalize
+		formatter = Formatter("%(name)s - %(levelname)s - %(asctime)s: %(message)s")
+		handler = StreamHandler(sys.stdout)
+		handler.setFormatter(formatter)
+		self.addHandler(handler)
